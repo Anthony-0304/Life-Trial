dd18cedb3334203327ea8b194c11b927e2da7f4e
becky lomax <becky.lomax@me.com>
2023-02-27 17:41:46 +0000

    Amended associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookings, :listings
+  has_many :bookings
+  has_many :listings
   has_many :reviews, through: :bookings
 
   validates :first_name, :last_name, :email, :password, presence: true
