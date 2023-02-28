6f81a49798bd825e5bb8078c5a6d4bb046bf2466
Kat WynneEllis <k.wynneellis@gmail.com>
2023-02-28 12:19:06 +0000

    connects cloudinary and builds the new listings form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -24,6 +24,6 @@ class ListingsController < ApplicationController
   private
 
   def listing_params
-    params.require(:listing).permit(:title, :category, :price, :description, :user_id)
+    params.require(:listing).permit(:title, :category, :price, :description, :user_id, :photos)
   end
 end
